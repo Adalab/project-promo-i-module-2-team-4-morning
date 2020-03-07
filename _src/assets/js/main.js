2f42a7ff1fa3e5e4b270bf7ec93d90d69621c045
violetagonza <violetagonzalezcalleja@gmail.com>
2020-03-07 19:55:35 +0100

    Add hidden event feature to design section

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const getDesignArrow = document.querySelector('.fa-angle-up');
+const getDesignSection = document.querySelector('.design--container__2');
+
+function hidSection(ev) {
+  getDesignSection.classList.toggle('hidden');
+  getDesignArrow.classList.toggle('closed');
+}
+console.log(hidSection);
+getDesignArrow.addEventListener('click', hidSection);
