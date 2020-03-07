5578d40d09b52accaabcd8444883248571ce9e28
violetagonza <violetagonzalezcalleja@gmail.com>
2020-03-07 20:20:58 +0100

    Add hidden feature to share ans fill sections

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,11 +1,33 @@
 'use strict';
-
-const getDesignArrow = document.querySelector('.fa-angle-up');
+//OCULTA SECCIÓN DESIGN
+const getDesignArrow = document.querySelector('.js-arrow');
 const getDesignSection = document.querySelector('.design--container__2');
 
-function hidSection(ev) {
+function hidDesignSection(ev) {
   getDesignSection.classList.toggle('hidden');
   getDesignArrow.classList.toggle('closed');
 }
-console.log(hidSection);
-getDesignArrow.addEventListener('click', hidSection);
+
+getDesignArrow.addEventListener('click', hidDesignSection);
+
+//OCULTA SECCIÓN FILL
+const getFillArrow = document.querySelector('.js-arrow-fill');
+const getFillSection = document.querySelector('.js-fill--div');
+
+function hidFillSection(ev) {
+  getFillSection.classList.toggle('hidden');
+  getFillArrow.classList.toggle('closed');
+}
+
+getFillArrow.addEventListener('click', hidFillSection);
+
+//OCULTA SECCIÓN SHARE
+const getShareArrow = document.querySelector('.js-arrow-share');
+const getShareSection = document.querySelector('.js-share--div');
+
+function hidShareSection(ev) {
+  getShareSection.classList.toggle('hidden');
+  getShareArrow.classList.toggle('closed');
+}
+
+getShareArrow.addEventListener('click', hidShareSection);
