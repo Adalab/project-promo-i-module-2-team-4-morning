dac6bb63287605f3eb963fa69b6e4caa2d78b565
Grisel Matos <griselmatosm@gmail.com>
2020-03-08 01:08:40 +0100

    Created even listener function for color palettes

---
+++ _src/assets/js/desig-section.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// data
+
+
+//listen events
+const selectPalette1 = document.querySelector('.js-radio-design-palette-1');
+const selectPalette2 = document.querySelector('.js-radio-design-palette-2');
+const selectPalette3 = document.querySelector('.js-radio-design-palette-3');
+
+function handlePreviewCard(event) {
+  const currentTarget = event.currentTarget;
+  if (currentTarget.classList.contains('js-radio-design-palette-1')) {
+    console.log('Han seleccionado la paleta 1');
+  } else if (currentTarget.classList.contains('js-radio-design-palette-2')) {
+    console.log('Han seleccionado la paleta 2');
+  } else if (currentTarget.classList.contains('js-radio-design-palette-3')) {
+    console.log('Han seleccionado la paleta 3');
+  }
+}
+
+selectPalette1.addEventListener('change', handlePreviewCard);
+selectPalette2.addEventListener('change', handlePreviewCard);
+selectPalette3.addEventListener('change', handlePreviewCard);
+
+//styles preview card
+
