f91334aaca9a76f2df301ec0fad50bcd1a3b8947
Martamp <motymont@gmail.com>
2020-03-08 15:52:25 +0100

    new branch for foill section JS

--- _src/assets/js/desig-section.js
+++ _src/assets/js/desig-section.js
@@ -1,27 +1,34 @@
-'use strict';
+"use strict";
 
 // data
 
-
 //listen events
-const selectPalette1 = document.querySelector('.js-radio-design-palette-1');
-const selectPalette2 = document.querySelector('.js-radio-design-palette-2');
-const selectPalette3 = document.querySelector('.js-radio-design-palette-3');
+const selectPalette1 = document.querySelector(".js-radio-design-palette-1");
+const selectPalette2 = document.querySelector(".js-radio-design-palette-2");
+const selectPalette3 = document.querySelector(".js-radio-design-palette-3");
 
 function handlePreviewCard(event) {
   const currentTarget = event.currentTarget;
-  if (currentTarget.classList.contains('js-radio-design-palette-1')) {
-    console.log('Han seleccionado la paleta 1');
-  } else if (currentTarget.classList.contains('js-radio-design-palette-2')) {
-    console.log('Han seleccionado la paleta 2');
-  } else if (currentTarget.classList.contains('js-radio-design-palette-3')) {
-    console.log('Han seleccionado la paleta 3');
+  if (currentTarget.classList.contains("js-radio-design-palette-1")) {
+    console.log("Han seleccionado la paleta 1");
+  } else if (currentTarget.classList.contains("js-radio-design-palette-2")) {
+    console.log("Han seleccionado la paleta 2");
+  } else if (currentTarget.classList.contains("js-radio-design-palette-3")) {
+    console.log("Han seleccionado la paleta 3");
   }
 }
 
-selectPalette1.addEventListener('change', handlePreviewCard);
-selectPalette2.addEventListener('change', handlePreviewCard);
-selectPalette3.addEventListener('change', handlePreviewCard);
+selectPalette1.addEventListener("change", handlePreviewCard);
+selectPalette2.addEventListener("change", handlePreviewCard);
+selectPalette3.addEventListener("change", handlePreviewCard);
 
 //styles preview card
 
+// Rellena section
+
+const nameBtn = document.querySelector(".js-fill-name");
+console.log(nameBtn);
+
+function changeCardInfo() {}
+
+nameBtn.addEventListener("click", changeCardInfo);
