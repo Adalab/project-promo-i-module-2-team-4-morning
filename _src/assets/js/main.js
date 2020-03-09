d7b99081196e8cc46ef300712ccb6182bdb338e5
smgaragorri <smgaragorri@gmail.com>
2020-03-09 13:39:40 +0100

    Rename functions on Reset Button  to be more descriptive

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -2,7 +2,7 @@
 
 const buttonReset = document.querySelector('.js-card-sample--button');
 
-function ConfirmDemo() {
+function confirmReset() {
   Swal.fire({
     html:
       ' <h2 class="swal2-title" id="swal2-title" style="display: flex; font-size:1.25em; font-family: merriweather;">Se van a borrar los cambios, ¿está seguro?</h2>',
@@ -12,19 +12,19 @@ function ConfirmDemo() {
     cancelButtonText: 'No, volver al editor',
     confirmButtonColor: '#28B463',
     confirmButtonText: 'Si, borrar cambios',
-    background: '#fff url(/images/trees.png)',
+    background: '#fff',
     backdrop: `
 			rgba(0,0,123,0.4)
 			url("../assets/images/nyan-cat-2.gif")
 			left top
 			no-repeat
 		`
-  }).then(hola);
+  }).then(resetForm);
 }
-function hola(result) {
+function resetForm(result) {
   if (result.value) {
     document.querySelector('.form').reset();
   }
 }
 
-buttonReset.addEventListener('click', ConfirmDemo);
+buttonReset.addEventListener('click', confirmReset);
