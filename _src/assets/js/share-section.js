8840bc538493f9d7ca66309df8c1a9ae70925885
violetagonza <violetagonzalezcalleja@gmail.com>
2020-03-14 20:05:54 +0100

    Add js feature to add or remove hidden class when all inputs are filled

--- _src/assets/js/share-section.js
+++ _src/assets/js/share-section.js
@@ -1,3 +1,4 @@
+//coge los inputs de texto (falta la imagen)
 const getInputName = document.querySelector('.js-fill-name');
 const getInputJob = document.querySelector('.js-fill-job');
 // const getInputImg = document.querySelector('.js-fill-file');
@@ -5,15 +6,19 @@ const getInputMail = document.querySelector('.js-fill-email');
 const getInputPhone = document.querySelector('.js-fill-tel');
 const getInputLinkedin = document.querySelector('.js-fill-linkedin');
 const getInputGithub = document.querySelector('.js-fill-github');
-
+//Coge el botón de comparte
 const btn = document.querySelector('.js-btn');
-// const getAllInputs = getInputGithub + getInputImg + getInputJob + getInputLinkedin + getInputMail + getInputPhone + getInputName;
-function addClassBtn() {
+
+// handle el botón de comparte
+function handleButton() {
   if (getInputGithub.value === '' || getInputJob.value === '' || getInputName.value === '' || getInputPhone.value === '' || getInputMail.value === '' || getInputLinkedin.value === '') {
     btn.classList.remove('js-share--button');
   } else {
     btn.classList.add('js-share--button');
+    const getCardCreated = document.querySelector('.js-card-created');
+    getCardCreated.classList.remove('hidden');
   }
 }
 
-btn.addEventListener('click', addClassBtn);
+//observa el botón de comparte
+btn.addEventListener('click', handleButton);
