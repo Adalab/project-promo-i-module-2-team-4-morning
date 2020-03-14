fb226a7a766bdbb3390843827c0358615644fc12
Grisel Matos <griselmatosm@gmail.com>
2020-03-14 18:27:40 +0100

    Change names in preview object

--- _src/assets/js/fill-section.js
+++ _src/assets/js/fill-section.js
@@ -21,35 +21,35 @@ let clientData = {
 let previewData = {
   palette: [],
   //llamar namePrev --- name
-  namePrev: document.querySelector('.js-card-sample--card__title'),
-  jobPrev: document.querySelector('.js-card-sample--card__subtitle'),
-  imgPrev: document.querySelector('.js-card-sample--card__img'),
-  emailPrev: document.querySelector('.js-email-preview'),
-  telPrev: document.querySelector('.js-tel-preview'),
-  linkedinPrev: document.querySelector('.js-linkedin-preview'),
-  githubPrev: document.querySelector('.js-github-preview')
+  name: document.querySelector('.js-card-sample--card__title'),
+  job: document.querySelector('.js-card-sample--card__subtitle'),
+  imgURL: document.querySelector('.js-card-sample--card__img'),
+  email: document.querySelector('.js-email-preview'),
+  tel: document.querySelector('.js-tel-preview'),
+  linkedin: document.querySelector('.js-linkedin-preview'),
+  github: document.querySelector('.js-github-preview')
 };
 
 function paintForm(ev) {
   if (clientData.name.value === '') {
-    previewData.namePrev.innerHTML = 'Nombre Apellido';
+    previewData.name.innerHTML = 'Nombre Apellido';
   } else {
-    previewData.namePrev.innerHTML = clientData.name.value;
+    previewData.name.innerHTML = clientData.name.value;
   }
   // if (ev.currentTarget.classList.contains("js-fill-name")) {
   // }
   if (ev.currentTarget.classList.contains('js-fill-job')) {
-    previewData.jobPrev.innerHTML = clientData.job.value;
+    previewData.job.innerHTML = clientData.job.value;
   } else if (ev.currentTarget.classList.contains('js-fill-file')) {
-    previewData.imgPrev.setAttribute('src', clientData.imgURL);
+    previewData.imgURL.setAttribute('src', clientData.imgURL);
   } else if (ev.currentTarget.classList.contains('js-fill-email')) {
-    previewData.emailPrev.setAttribute('href', `mailto: ${clientData.email.value}`);
+    previewData.email.setAttribute('href', `mailto: ${clientData.email.value}`);
   } else if (ev.currentTarget.classList.contains('js-fill-tel')) {
-    previewData.telPrev.setAttribute('href', `tel: +34 ${clientData.tel.value}`);
+    previewData.tel.setAttribute('href', `tel: +34 ${clientData.tel.value}`);
   } else if (ev.currentTarget.classList.contains('js-fill-linkedin')) {
-    previewData.linkedinPrev.setAttribute('href', clientData.linkedin.value);
+    previewData.linkedin.setAttribute('href', clientData.linkedin.value);
   } else if (ev.currentTarget.classList.contains('js-fill-github')) {
-    previewData.githubPrev.setAttribute('href', clientData.github.value);
+    previewData.github.setAttribute('href', clientData.github.value);
   }
 }
 clientData.name.addEventListener('keyup', paintForm);
@@ -82,10 +82,10 @@ selectPalette3.addEventListener('change', paintColorsPalette);
 const linksCard = document.querySelectorAll('.js-card-sample--item__link');
 
 function addPalette1() {
-  previewData.namePrev.classList.add('title--palette-1');
-  previewData.namePrev.classList.remove('title--palette-2', 'title--palette-3');
-  previewData.jobPrev.classList.add('subtitle--palette-1');
-  previewData.jobPrev.classList.remove('subtitle--palette-2', 'subtitle--palette-3');
+  previewData.name.classList.add('title--palette-1');
+  previewData.name.classList.remove('title--palette-2', 'title--palette-3');
+  previewData.job.classList.add('subtitle--palette-1');
+  previewData.job.classList.remove('subtitle--palette-2', 'subtitle--palette-3');
   for (let i = 0; i < linksCard.length; i++) {
     linksCard[i].classList.add('link--palette-1');
     linksCard[i].classList.remove('link--palette-2', 'link--palette-3');
@@ -93,10 +93,10 @@ function addPalette1() {
 }
 
 function addPalette2() {
-  previewData.namePrev.classList.add('title--palette-2');
-  previewData.namePrev.classList.remove('title--palette-1', 'title--palette-3');
-  previewData.jobPrev.classList.add('subtitle--palette-2');
-  previewData.jobPrev.classList.remove('subtitle--palette-1', 'subtitle--palette-3');
+  previewData.name.classList.add('title--palette-2');
+  previewData.name.classList.remove('title--palette-1', 'title--palette-3');
+  previewData.job.classList.add('subtitle--palette-2');
+  previewData.job.classList.remove('subtitle--palette-1', 'subtitle--palette-3');
   for (let i = 0; i < linksCard.length; i++) {
     linksCard[i].classList.add('link--palette-2');
     linksCard[i].classList.remove('link--palette-1', 'link--palette-3');
@@ -104,10 +104,10 @@ function addPalette2() {
 }
 
 function addPalette3() {
-  previewData.namePrev.classList.add('title--palette-3');
-  previewData.namePrev.classList.remove('title--palette-1', 'title--palette-2');
-  previewData.jobPrev.classList.add('subtitle--palette-3');
-  previewData.jobPrev.classList.remove('subtitle--palette-1', 'subtitle--palette-2');
+  previewData.name.classList.add('title--palette-3');
+  previewData.name.classList.remove('title--palette-1', 'title--palette-2');
+  previewData.job.classList.add('subtitle--palette-3');
+  previewData.job.classList.remove('subtitle--palette-1', 'subtitle--palette-2');
   for (let i = 0; i < linksCard.length; i++) {
     linksCard[i].classList.add('link--palette-3');
     linksCard[i].classList.remove('link--palette-1', 'link--palette-2');
